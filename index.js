21342e26f8fd5f0f6a0d6702ecc86fbe76816360
acehnery <92311620+acehnery@users.noreply.github.com>
2022-01-17 19:15:09 +0630

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+// import modules
+require("dotenv").config();
+const { Client,Intents } = require('discord.js');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS, //Access to guild which is server
+        Intents.FLAGS.GUILD_MESSAGES //Give access to messages inside of our client(bot) so inside of a guild we will be able to guild messages
+    ]
+});
+
+// Event 
+client.once("ready", () => {         //creates an arrow function that contains console.log("Bot is ready.");
+    console.log("Bot is ready.");
+});
+
+// Command
+client.on("messageCreate", message => {
+    if (message.content.startsWith('!')) {
+        if (message.content.substring(1) === "ping") {
+            message.reply("Pong!")
+        }
+    }
+})
+
+client.login(process.env.TOKEN); //You can skip dotenv part and just make a string eg. client.log('YOUR_TOKEN'), create .env file declare the token variable eg. TOKEN=YOUR_TOKEN
